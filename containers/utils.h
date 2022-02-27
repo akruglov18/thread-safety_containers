fd8112c270efa8287bee72d0bb7dd81667d7452e
akruglov18 <a.kruglov.2001@mail.ru>
2022-02-27 14:54:47 +0300

    code style fixes

--- containers/utils.h
+++ containers/utils.h
@@ -2,10 +2,7 @@
 #include <type_traits>
 #include <utility>
 
-template< class T >
-struct is_atomic
-    : std::integral_constant<
-    bool,
-    std::is_same<bool, typename std::remove_cv<T>::type>::value ||
-    std::is_same<int, typename std::remove_cv<T>::type>::value
-    > {};
+template <class T>
+struct is_atomic :
+        std::integral_constant<bool, std::is_same<bool, typename std::remove_cv<T>::type>::value ||
+                                             std::is_same<int, typename std::remove_cv<T>::type>::value> {};

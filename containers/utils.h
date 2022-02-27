f7de8a998e093fca2e71807f6617af58b55b2392
akruglov18 <a.kruglov.2001@mail.ru>
2022-02-27 14:38:04 +0300

    Added is_atomic

---
+++ containers/utils.h
@@ -0,0 +1,11 @@
+#include <atomic>
+#include <type_traits>
+#include <utility>
+
+template< class T >
+struct is_atomic
+    : std::integral_constant<
+    bool,
+    std::is_same<bool, typename std::remove_cv<T>::type>::value ||
+    std::is_same<int, typename std::remove_cv<T>::type>::value
+    > {};

9398fc84eec883b99bbe4cb633ab85fc0c6a95c4
Aleksei Kruglov <aleksei.kruglov@intel.com>
2022-03-03 19:36:45 +0300

    added vector (#3)

--- containers/utils.h
+++ containers/utils.h
@@ -5,4 +5,4 @@
 template <class T>
 struct is_atomic :
         std::integral_constant<bool, std::is_same<bool, typename std::remove_cv<T>::type>::value ||
-                                             std::is_same<int, typename std::remove_cv<T>::type>::value> {};
+                               std::is_same<int, typename std::remove_cv<T>::type>::value> {};
